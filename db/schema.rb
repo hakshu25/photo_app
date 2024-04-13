fe2a7e46c1d0ee54c990c7c5b6d27f9b1b755d9f
hakshu25 <hakshu25@gmail.com>
2024-04-13 14:58:00 +0900

    db: アップロードする写真を紐づけるため users テーブルをマイグレーションで追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_13_052946) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_13_054046) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_13_052946) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "account_id"
+    t.string "password_digest"
+    t.datetime "created_at"
+    t.index ["account_id"], name: "index_users_on_account_id", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
