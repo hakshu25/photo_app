9c3a74fb60b83aac4143d2a00b7dd6cd37253287
hakshu25 <hakshu25@gmail.com>
2024-04-13 17:54:24 +0900

    db: アップロードする写真のタイトルを保持できるよう photographs テーブルをマイグレーションで追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_13_054046) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_13_085057) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_13_054046) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "photographs", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "title"
+    t.datetime "created_at"
+    t.index ["user_id"], name: "index_photographs_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "account_id"
     t.string "password_digest"
@@ -51,4 +58,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_13_054046) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "photographs", "users"
 end
