af749ec220d8e8cb2d6b3ed725cddd86a9d15c2f
hakshu25 <hakshu25@gmail.com>
2024-04-14 11:11:31 +0900

    feat: ユーザーモデルにアカウントIDとパスワードのバリデーションを追加した

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :photographs
+
+  validates :account_id, presence: true, uniqueness: true
+  validates :password, presence: true
 end
